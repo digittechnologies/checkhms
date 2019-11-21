ba7522e9b3b13fc83cae91e18d073ada018b60b6
goldenglorys <olusolagloryolamide@gmail.com>
2019-11-21 10:17:47 +0100

    changePassword controller added

---
+++ backend/app/Http/Controllers/ChangePasswordController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\User;
+
+class ChangePasswordController extends Controller
+{
+    public function process(Request $request)
+    {
+    	$token = DB::table('password_resets')
+    	->where(['email' => $request->email, 'token' => $request->token])->first();
+    	if(!empty($token)){
+    		$pawd = '';
+    		$update = DB::table('users')->where('email', '=', $request->email)
+    		->update([
+	            'password'=> bcrypt($request->password)
+        	]);
+    	 	if($update){
+	            return '{
+	                "success":true,
+	                "message":"successful"
+	            }' ;
+	        } else {
+	            return '{
+	                "success":false,
+	                "message":"Failed"
+	            }';
+	        }
+	    }
+    	 if(empty($token)){
+    	 	return '{
+                "success":false,
+                "message":"Failed"
+            }';
+    	 }
+    }
+
+}
