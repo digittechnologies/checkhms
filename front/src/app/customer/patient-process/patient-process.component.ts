0810f0a358097f84999a9a229db885e733b2b9a4
Olamidun21 <delightolarinde@gmail.com>
2020-05-26 17:26:26 +0100

    switching branch to master

--- front/src/app/customer/patient-process/patient-process.component.ts
+++ front/src/app/customer/patient-process/patient-process.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { JarwisService } from 'src/app/service/jarwis.service';
+import { TokenService } from 'src/app/service/token.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
+import { MatSnackBar } from '@angular/material';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-patient-process',
@@ -7,9 +13,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientProcessComponent implements OnInit {
 
-  constructor() { }
+  appId: string;
+  patientResponse: any;
+  pat: any;
+  patID: any;
+  schemeCat: any;
+  schemeId: any;
+  schemePercent: any;
+  schemePercentToView: number;
+  schemePriceList: any;
+  response: any;
+  imgLink: any;
+
+  constructor(   private Jarwis: JarwisService,
+    private Token: TokenService,
+    private router: Router,
+    private Auth: AuthService,
+    public snackBar: MatSnackBar, 
+    public actRoute: ActivatedRoute,) { }
 
   ngOnInit() {
+
+    this.actRoute.paramMap.subscribe((params => {
+	    let id = params.get('id');
+	    this.appId= id;
+	    this.Jarwis.patientdetails(id).subscribe(
+	      data=>{
+	      this.patientResponse = data;      
+        this.pat = this.patientResponse;
+        this.patID = this.pat[0].id;
+        this.schemeCat = this.pat[0].category_name;
+        this.schemeId = this.pat[0].n_h_i_s;
+        this.schemePercent = this.pat[0].pacentage_value;
+        this.schemePercentToView = 100 -this.pat[0].pacentage_value;
+        this.schemePriceList = this.pat[0].price_list_column;
+	    })
+  }))
+  
+  this.Jarwis. generalSettings().subscribe(
+    data=>{
+    this.response = data;      
+    this.imgLink = this.response[0].app_url;
+  })
   }
 
 }
